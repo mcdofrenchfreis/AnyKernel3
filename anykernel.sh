478bfc5dfd06b42f73953639a76642562ef46b16
Danny Lin <danny@kdrag0n.dev>
2021-03-14 13:46:07 +0000

    AnyKernel: Skip ramdisk unpacking and repacking
    We don't currently modify anything in the ramdisk, so skip these steps
    to speed up the flashing process.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- anykernel.sh
+++ anykernel.sh
@@ -37,9 +37,10 @@ patch_vbmeta_flag=auto;
 . tools/ak3-core.sh;
 
 # boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+split_boot;
 
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+flash_boot;
+flash_dtbo;
 ## end boot install
 
 
